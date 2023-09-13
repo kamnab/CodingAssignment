dba08791aee414a7aefaf01e6ecfeefb8cfd6fe7
Kamnab <Kamnab@DESKTOP-UETSVQ2>
2023-09-13 20:10:15 +0700

    Add project files.

---
+++ CodingAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿var list = new List<string>()
+{
+    "BE","FE","QA","Urgent"
+};
+
+Func<string, List<string>> Input = (input) =>
+{
+    var output = new List<string>();
+
+    list.ForEach(x =>
+    {
+        if (input.Contains($"[{x}]")) output.Add(x);
+    });
+
+    return output;
+};
+
+var input = "[BE][FE][Urgent] there is error";
+Console.WriteLine(input);
+Console.WriteLine($"Receive channels: {string.Join(", ", Input(input))}{Environment.NewLine}");
+
+input = "[BE][QA][HAHA][Urgent] there is error";
+Console.WriteLine(input);
+Console.WriteLine($"Receive channels: {string.Join(", ", Input(input))}");
+
+Console.ReadLine();
